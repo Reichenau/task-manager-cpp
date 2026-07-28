e45d9fa80d8733137457d796493ad6cb57d6687e
Reichenau <schildt515@gmail.com>
2026-07-28 21:17:07 +0500

    feat: add TaskManager class

---
+++ include/TaskManager.hpp
@@ -0,0 +1,20 @@
+#pragma once
+#include <vector>
+#include <string>
+
+#include "TaskManager.hpp"
+#include "Task.hpp"
+
+class TaskManager{
+private:
+    std::vector<Task> m_tasks;
+public:
+    void addTask(const std::string& title, const std::string& desc);
+    void removeTask(std::size_t id);
+    void completeTask(std::size_t id);
+    
+    std::vector<Task> getTasks() const {return m_tasks;}
+    std::vector<Task> getPending() const;
+    std::vector<Task> getCompleted() const;
+    void clear();
+};
